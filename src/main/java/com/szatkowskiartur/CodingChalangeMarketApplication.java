1cbc761a2de174bf6dd35f8837bcf2749ae5fec4
Artur <szatkowski.artur.iii@gmail.com>
2021-12-07 11:58:59 +0100

    First Commit

---
+++ src/main/java/com/szatkowskiartur/CodingChalangeMarketApplication.java
@@ -0,0 +1,13 @@
+package com.szatkowskiartur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodingChalangeMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodingChalangeMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/szatkowskiartur/CodingChalangeMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.szatkowskiartur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodingChalangeMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
